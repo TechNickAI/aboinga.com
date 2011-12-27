47c3eb697196199f1d699520a62273bec2a46888
Nick Sullivan <nick@sullivanflock.com>
2011-12-27 01:48:39 -0800

    change upload_ip to IPAddressField (didn't know that existed)

--- aboinga_web/moments/models.py
+++ aboinga_web/moments/models.py
@@ -4,7 +4,7 @@ class Moment(models.Model):
 
     slug = models.CharField(max_length = 255, db_index = True, unique = True)
     filename = models.CharField(max_length = 255, db_index = True, unique = True)
-    upload_ip = models.CharField(max_length = 15, null = True, blank = True)
+    upload_ip = models.IPAddressField(null = True, blank = True)
     created_at = models.DateTimeField(auto_now_add = True, db_index = True)
     updated_at = models.DateTimeField(auto_now_add = True, auto_now = True)
     expires = models.DateTimeField(null = True, blank = True)
