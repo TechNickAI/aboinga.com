e0e0fcd653e0a13630b8ac0514a15342e257fe37
Nick Sullivan <nick@sullivanflock.com>
2011-12-28 15:32:51 -0800

    display uploaded time

--- aboinga_web/moments/api.py
+++ aboinga_web/moments/api.py
@@ -1,6 +1,7 @@
 from aboinga_web.moments.models import Moment
 from tastypie.api import Api
 from tastypie.resources import ModelResource
+import repoze.timeago
 
 class MomentResource(ModelResource):
 
@@ -13,6 +14,14 @@ class MomentResource(ModelResource):
         ordering = ['created_at']
         queryset = Moment.objects.all()
         resource_name = 'moment'
+
+    def dehydrate(self, bundle):
+        # Human readable times
+        bundle.data["created_at_ago"] = repoze.timeago.get_elapsed(bundle.obj.created_at)
+        bundle.data["updated_at_ago"] = repoze.timeago.get_elapsed(bundle.obj.updated_at)
+        bundle.data["expires_in"] = repoze.timeago.get_elapsed(bundle.obj.expires)
+
+        return bundle
 
 # This is called from urls.py
 ABOINGA_API = Api(api_name='v1')
