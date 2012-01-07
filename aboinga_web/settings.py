07e6c04b6a9ee935cb2dcd5ae8666162c347b0a8
Nick Sullivan <nick@sullivanflock.com>
2012-01-07 10:01:16 -0800

    custom templates dir. head_tags template for 3rd party tags

--- aboinga_web/settings.py
+++ aboinga_web/settings.py
@@ -144,6 +144,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ROOT_PATH + '/custom_templates'
 )
 
 INSTALLED_APPS = (
