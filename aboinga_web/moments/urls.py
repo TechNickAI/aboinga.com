7c3269961c82ddb35779b9d5202c70732d23ddbd
Nick Sullivan <nick@sullivanflock.com>
2012-09-07 23:18:57 -0700

    about page working

--- aboinga_web/moments/urls.py
+++ aboinga_web/moments/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     # One for public, one for private
     (r'^moment/not-public/(?P<slug>.*)$', 'aboinga_web.moments.views.detail', {'public_path': False}),
     (r'^moment/(?P<slug>.*)$', 'aboinga_web.moments.views.detail', {'public_path': True}),
+    (r'^about', 'aboinga_web.moments.views.about'),
     (r'^$', 'aboinga_web.moments.views.home'),
 )
--- aboinga_web/moments/views.py
+++ aboinga_web/moments/views.py
@@ -5,6 +5,9 @@ from django.template import RequestContext
 def home(request):
     return render_to_response("home.html", {}, RequestContext(request))
 
+def about(request):
+    return render_to_response("about.html", {}, RequestContext(request))
+
 def detail(request, slug, public_path = True):
     moment = get_object_or_404(Moment, slug = slug, public = public_path)
     return render_to_response("detail.html", {"moment": moment, "moment_title": moment.title}, RequestContext(request))
